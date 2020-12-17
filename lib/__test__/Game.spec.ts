4ad55e8ce39c010f7cabc027ce887825b6762df3
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2020-12-17 19:28:32 +0200

    test that it shows the a messages if there is a draw on the board

--- lib/__test__/Game.spec.ts
+++ lib/__test__/Game.spec.ts
@@ -85,6 +85,16 @@ test('it shows the a messages if a players does not restart the game', async () 
   expect(showFunctionSpy).toHaveBeenCalledWith(messages.thankYou());
 });
 
+test('it shows the a messages if there is a draw on the board', async () => {
+  const grid = ['X', 'X', 'O', 'O', 'O', 'X', 'X', 'O', ''];
+  const { game, display, messages } = setup(grid, ['9'], [false]);
+
+  const showFunctionSpy = jest.spyOn(display, 'show').mockImplementation();
+  await game.playGame();
+
+  expect(showFunctionSpy).toHaveBeenCalledWith(messages.drawGame());
+});
+
 class DisplayMock implements Display {
   moves: string[];
   replay: boolean[];
