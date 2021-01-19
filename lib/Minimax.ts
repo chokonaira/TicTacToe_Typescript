c01de73494af27b33fe7896bc9b2587f8c73b023
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2021-01-19 09:44:35 +0200

    refactored the minimax funtion added one test case

--- lib/Minimax.ts
+++ lib/Minimax.ts
@@ -8,6 +8,23 @@ class Minimax {
     this.opponent = opponent;
   }
 
+  findBestMove(board: Board): number {
+    let bestValue = -1000;
+    let bestMove = 0;
+    board.availablePositions().forEach((position) => {
+      const defaultPositionState = board.defaultPositionState(position);
+      board.makeMove(position, this.currentPlayer);
+      const moveValue = this.miniMax(board, 0, false);
+      board.makeMove(position, defaultPositionState);
+
+      if (moveValue > bestValue) {
+        bestMove = position;
+        bestValue = moveValue;
+      }
+    });
+    return bestMove;
+  }
+
   evaluate(board: Board, depth = 0): number {
     if (board.winningPlayer() === this.currentPlayer) {
       return 10 - depth;
@@ -37,28 +54,11 @@ class Minimax {
       board.availablePositions().forEach((position) => {
         const defaultPositionState = board.defaultPositionState(position);
         board.makeMove(position, this.opponent);
-        bestValue = Math.min(bestValue, this.miniMax(board, depth + 1, isMax));
+        bestValue = Math.min(bestValue, this.miniMax(board, depth + 1, !isMax));
         board.makeMove(position, defaultPositionState);
       });
       return bestValue;
     }
-  }
-
-  findBestMove(board: Board): number {
-    let bestValue = -1000;
-    let bestMove = 0;
-    board.availablePositions().forEach((position) => {
-      const defaultPositionState = board.defaultPositionState(position);
-      board.makeMove(position, this.currentPlayer);
-      const moveValue = this.miniMax(board, 0, false);
-      board.makeMove(position, defaultPositionState);
-
-      if (moveValue > bestValue) {
-        bestMove = position;
-        bestValue = moveValue;
-      }
-    });
-    return bestMove;
   }
 }
 
