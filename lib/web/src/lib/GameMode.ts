0a7b2ebade79e6b162a8af0f2863571fcd06665a
Henry Okonkwo <okonkwo.henry2012@gmail.com>
2021-02-02 16:10:05 +0200

    refactored GameMode to remove type error

---
+++ lib/web/src/lib/GameMode.ts
@@ -0,0 +1,25 @@
+// import HumanPlayer from '../../../players/HumanPlayer';
+import BeatablePlayer from '../../../players/BeatablePlayer';
+import UnbeatablePlayer from '../../../players/UnbeatablePlayer';
+import Minimax from '../../../Minimax';
+import RandomChoice from '../../../RandomChoice';
+import { Player } from '../../../interfaces/Player';
+
+export interface Mode {
+  1: UnbeatablePlayer;
+  2: BeatablePlayer;
+  [key: number]: UnbeatablePlayer | BeatablePlayer;
+}
+
+class GameMode {
+
+  modeType(mode: number): Player {
+    const gameModeType: Mode = {
+      1: new UnbeatablePlayer(new Minimax('X', 'O')),
+      2: new BeatablePlayer(new RandomChoice())
+    };
+    return gameModeType[mode];
+  }
+}
+
+export default GameMode;
